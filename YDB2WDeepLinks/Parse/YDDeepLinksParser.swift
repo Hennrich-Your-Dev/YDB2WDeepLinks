c46345b07bb4d87d97bed4fa759031fb9f7dacd7
Douglas Hennrich <douglashennrich@yourdev.com.br>
2021-05-21 14:09:54 -0300

    Iniciando parser de Deeplinks

--- YDB2WDeepLinks/Parse/YDDeepLinksParser.swift
+++ YDB2WDeepLinks/Parse/YDDeepLinksParser.swift
@@ -11,17 +11,40 @@ import YDExtensions
 
 public enum YDDeepLinksParser {
   // MARK: Enum
-  public enum KnowedPaths: String {
+  public enum Types: String {
     case nps
   }
 
   // MARK: Actions
-  public static func parse(url: URL) {
+  public static func parse(url: URL) -> (
+    path: YDDeepLinksParserTypes?,
+    parameters: [String: Any]?
+  ) {
     let allComponents = url.host.map { [$0] + url.pathComponents } ?? url.pathComponents
     let components = allComponents
       .filter { $0 != "/" }
       .map { $0.removingPercentEncoding ?? "" }
 
-    print(components)
+    var params: [String: String] = [:]
+
+    if let parameters = url.queryParameters {
+      for (key, value) in parameters {
+        params[key.lowercased()] = value
+      }
+    }
+
+    return (
+      path: YDDeepLinksParserTypes(rawValue: components.last ?? ""),
+      parameters: params
+    )
   }
+}
+
+public enum YDDeepLinksParserTypes: String {
+  case nps
+}
+
+public enum YDDeepLinksParserParameters: String {
+  case storeId = "storeid"
+  case eTag = "etag"
 }
